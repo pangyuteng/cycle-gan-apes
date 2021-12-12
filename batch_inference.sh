985e78f14c287ffa9e5e6b7596780f2a3f2f4ac0
pangyuteng <pangyuteng@gmail.com>
2021-12-12 11:26:48 -0800

    added batch inference

--- batch_inference.sh
+++ batch_inference.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+curl https://booksoficons.com/wp-content/uploads/2020/11/jason-calacanis.jpg --output static/jason-calacanis.jpg
+curl https://i.pinimg.com/280x280_RS/56/81/1f/56811f7936a5187625b25893fe47fcbe.jpg --output static/chamath-palihapitiya.jpg
+curl https://gossipgist.com/uploads/31116/david-friedberg-famous-for.png --output static/david-friedberg.png
+curl https://upload.wikimedia.org/wikipedia/commons/d/da/David_O._Sacks.jpg --output static/david-sacks.jpg
+
 CUDA_VISIBLE_DEVICES=1 python inference.py static/jason-calacanis.jpg static/ape-calacanis.png
 CUDA_VISIBLE_DEVICES=1 python inference.py static/david-friedberg.png static/ape-friedberg.png
 CUDA_VISIBLE_DEVICES=1 python inference.py static/chamath-palihapitiya.jpg static/ape-palihapitiya.png
